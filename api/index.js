46a441b920cadf8fbe1bb3fae6d7cbe6530b7375
pruebasgit <pruebasgitdn@gmail.com>
2024-03-24 11:48:10 -0500

    conect to batabase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+// Dotenv Carga las variables de entorno del archivo .env
+import dotenv from "dotenv";
+
+// config. carga los documentos .env
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Conected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 const app = express();
 app.listen(3000, () => {
   console.log("Servidor escuchando en puerto 3000");
