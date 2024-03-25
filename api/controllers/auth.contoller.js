3792d1354e1de46f5f8306911dfe8c33eccbecc2
pruebasgit <pruebasgitdn@gmail.com>
2024-03-25 18:10:35 -0500

    sign in api route

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
-
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 /*
 next del middleaware
 */
@@ -14,6 +15,53 @@ export const signup = async (req, res, next) => {
     // Esperar que guarde en la db
     await newUser.save();
     res.json("Usuario creado con exito!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const singin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    /*
+    Aca buscamos en la bd en el campo email: el email del req body
+    */
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return next(errorHandler(404, "Usuario no encontrado"));
+    }
+
+    /*
+    comparesync() => compara las contraseeñas y devuelve un booleano
+    */
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Contraseña invalida"));
+    }
+
+    /*
+    Si pasa las validaciones de usuario y contraseña ... creamos el token
+
+    payload: carga de datos json
+    secret key = llave que solo el servidor conoce
+
+
+    sign(payload,secret key)
+    */
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // Retornar todas las cosas menos la contraseña
+    const { password: pass, ...rest } = validUser._doc;
+
+    /*
+    Mandamos token a las cookies
+    res.cookie(nombrel token, token, opciones{})
+    */
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
