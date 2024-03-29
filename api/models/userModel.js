626953a43df40fa073d85728fd22491259a88b9d
pruebasgit <pruebasgitdn@gmail.com>
2024-03-29 18:46:27 -0500

    subir imagen completado

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default:
-        "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F89%2F90%2F48%2F899048ab0cc455154006fdb9676964b3.jpg&tbnid=ayWNELQUboo9qM&vet=12ahUKEwjkw4ex6ZeFAxWuj7AFHTuvB0MQxiAoA3oECAAQKg..i&imgrefurl=https%3A%2F%2Fwww.pinterest.es%2Fpin%2F606015693604756163%2F&docid=HixZqvfpNv2HkM&w=640&h=640&itg=1&q=user%20avatar&ved=2ahUKEwjkw4ex6ZeFAxWuj7AFHTuvB0MQxiAoA3oECAAQKg",
+        "https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg",
     },
   },
   { timestamps: true }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,80 @@
 import { useSelector } from "react-redux";
-
+import { useRef, useState, useEffect } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase.js";
 export const Profile = () => {
   /*
   Traemos el current user que es un estado del slice user que guarda al usuario que se le manda al formulario de ingreso en el reductor signinsucces .. y proviene del user slice 
   */
   const { currentUser } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [UploadError, setUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  console.log(filePerc);
+  console.log(file);
+  console.log(formData);
+  console.log(UploadError);
+
+  // useRef es un Hook de React que te permite referenciar un valor que no es necesario para el renderizado.
+  const fileRef = useRef(null);
+
+  // firebase storage nos permite almacenar arhivos generados por los usuarios
+  // request.resource.size = que el peso del recurso (menos de 2 mb), si no no carga
+  // request.resource.contentType.matches = que el contenido sea (imagenes)
+
+  // allow read;
+  // allow write: if
+  // request.resource.size < 2 * 1024 * 1024 &&
+  // request.resource.contentType.matches('image/.*')
+
+  // Si hay un archivo ejecutar en handldefileupload
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    // getstorage = Obtiene una instancia de Firebase StorageService para la aplicación Firebase dada.
+    const storage = getStorage(app);
+    // El nombre del archivo lo hacemos unico con la fecha + el nombre del archivo selecionado
+    const filename = new Date().getTime() + file.name;
+    // El storageref es la referencia de un objeto que puede ser cargado descargado establecido y eliminado en una ruta, le pasamos el storage, y el archivo
+    const storageRef = ref(storage, filename);
+    // uploadBytesResumable()=> Carga datos en la ubicación de este objeto. La carga se puede pausar y reanudar y expone actualizaciones de progreso.
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    // captamos el cambio con el on (en) state_changed (estado cambiado) hacer => {}
+    // snapshots piezas de informacion de cada estado cambiado cuando el progreso cambie
+    uploadTask.on(
+      "state_changed",
+      (snapshots) => {
+        // progreso es igual a los bytes transferidos a cada snapshot dividido los bytes totlaes por 100 para el porcentaje
+        const progress =
+          (snapshots.bytesTransferred / snapshots.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setUploadError(true);
+        console.log(error);
+      },
+      () => {
+        // getDownloadURL obtener la URL de descarga de un archivo
+        // Nos devuelve una promesa y en caso de de exito la pasamas a downloaURL que la guardamos en setFormData
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
+
   return (
     /*
     max-w-lg	= max-width: 32rem; (ancho maximo de 32)
@@ -12,11 +82,38 @@ export const Profile = () => {
     <div className="max-w-xl mx-auto p-3 ">
       <h1 className="text-3xl font-semibold text-center my-7">Perfil</h1>
       <form action="" className="flex flex-col gap-3">
+        {/* Escondemos el input y hacemos que se mustre con el ref y la fucion onclick de la img y que acepte solo imagenes*/}
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          id=""
+          hidden
+          accept="image/*"
+        />
+        {/* En source de image creamos condicional del actualizar imagen, si formData.avatar(que es la imagen que mandamos por el input file hidden) existe mostrarlo, de otra manera el currentUser.avatar (que es la que tiene sin actualizar)*/}
         <img
-          src={currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="perfil foto"
           className="rounded-full h-[150px] w-[150px] object-cover self-center cursor-pointer mt-2"
+          // En esta funcion al hacer click abrimos la referencia del input file que esta hidden
+          onClick={() => fileRef.current.click()}
         />
+        <p className="text-center text-sm">
+          {/* Si hay un error: mostrar el span de texto rojo, de otra manera el span cargando, pero si ese porcentaje es igual a 100 mostrar el span verde, de otra manera una coma vacia */}
+          {UploadError ? (
+            <span className="text-red-700">
+              {" "}
+              Error en Imagen seleccionada (debe ser menor a 2MB)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700">{`Subiendo imagen, ${filePerc}% completado.`}</span>
+          ) : filePerc == 100 ? (
+            <span className="text-green-700">Imagen subida con exito!!</span>
+          ) : (
+            ""
+          )}
+        </p>
         {/*El id de los inputs llevan el mismo nombre de los campos del modelo user, para targetearlos y añadirles el value (.e.target -- e.target.value) */}
         <input
           type="text"
